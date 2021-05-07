0aba704a629197389b758c3069c8b5e6724a5786
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-07 11:31:47 +0530

    Create vehicle.php
    add

---
+++ vehicle.php
@@ -0,0 +1,13 @@
+{
+  "result": "true",
+  "vehicle": [
+    {
+      "vehicle_id": 1,
+      "vehicle_no": "LX-3333"
+    },
+      {
+      "vehicle_id": 2,
+      "vehicle_no": "GX-2234"
+    },
+  ]
+}
