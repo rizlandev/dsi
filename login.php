a57b2bc592aacfd8d3abd79763436ff7784380eb
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-07 11:50:22 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,16 @@
+{
+    "result": true,
+    "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImY1NWVlZTIyMzk0NGYyYzg2NzgyNDkzOTUyY2I5MmEwNGEzZTdmOGZjMjEzZjU5MDFiYzA4MjFlNWY1YWEzNDdiYzc4MTU3ZjY5NDQzMzc2In0.eyJhdWQiOiIxIiwianRpIjoiZjU1ZWVlMjIzOTQ0ZjJjODY3ODI0OTM5NTJjYjkyYTA0YTNlN2Y4ZmMyMTNmNTkwMWJjMDgyMWU1ZjVhYTM0N2JjNzgxNTdmNjk0NDMzNzYiLCJpYXQiOjE1OTcxNDM4MjUsIm5iZiI6MTU5NzE0MzgyNSwiZXhwIjoxNjI4Njc5ODI1LCJzdWIiOiIxNDEiLCJzY29wZXMiOltdfQ.GqZEqpf6RVOLHQ0yIs5xLP_FJcRJSiFfWx5cr2QxpJipdem6b1MjKMpmvGdxZ31D4iaiAK-2TNH3hUPi0n6mDpIjafFUePZaakChvcMCO9HI3D2WosDIwtll5CLoqBOLHty6dixkrnL2BsMnNq0RShEIvEgOEIIkzz2mmSyn9GmlY4JY3UBw17c3VBByrLEOBPZUg_1P-LAzfyqGPeaaJKukOKOkgJdrdGx_-Vff0ZLHK_gkeCZkEMUG_FYQ7oIE7bbagD08_pvVrcN_4d86B0hDuJdtr6yDejHqL2HXpZFNFRM6jqLWpv1e4ADacc8_ODO1ANFI63oOqoJ9wvhNJAN-xvDiNZmHBn1AYfMYWlFoc2fuD3AaoqMJr8PIWE9WQXbQibhWS--iWASTH9JU60X3LEMqQr_VJsjcV_aHCd09wG_6wRSYp3y5c-GlcapC1K0w4WK-fABM-t8nqWZwMR3w7j2w0wUJrju554BkCLQD0XTYq68LikMG0ThV6zUQkesW4OWWOuouKSgxKUxzgy9YfigN8s3JVtW0c4q0kbeY9bLBtd0BMPFNhsNlunERFUWoLZmyNrJiAwUqA6YvJ6mPTWoXkie5CCVPjVKdSZfUxt1fUpsN599WuvI7k01jOcOT-Z6KI-sulsVJ6YV8rWTBs_IoGJ8ZGlXtZbrH-nE",
+    "name": "MR.RAMYASIRI",
+    "picture": null,
+    "phoneNumber": null,
+    "email": "RAMYASIRI",
+    "hasItinerary": true,
+    "userId": 141,
+    "last_sales_od_id": "DSI/SO/141/000018",
+    "last_invoice_id": "DSI/INV/141/000035",
+    "last_return_note_id": null,
+    "timestamp": 1597143825,
+    "loading_confirm_status": 1,
+    "session_id": 4
+}
