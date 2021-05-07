24ae912d7be86cc4bb070f908f64e15ae382968e
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-07 12:04:53 +0530

    Update and rename vehicle.php to vehicle_details.php
    updated

---
+++ vehicle_details.php
@@ -0,0 +1,15 @@
+{
+  "result": true,
+  "vehicles": [
+    {
+      "vehi_id": 1,     
+      "vehi_code": "001",  
+      "vehi_no": "LX-3456"
+    },
+    {
+      "vehi_id": 2,
+      "vehi_code": "002",
+      "vehi_no": "LX-5689"
+    }
+  ]
+}
