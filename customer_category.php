1c8967513ab04f3b6892fc533da321f1f975f55a
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-19 13:12:04 +0530

    Create customer_category.php

---
+++ customer_category.php
@@ -0,0 +1,33 @@
+{
+  "result": true,
+  "customer_category": [
+    {
+      "category_id": 1,
+      "category_name": "CATEGORY_1",
+      "sub_category": [
+        {
+          "sub_cat_id": 1,
+          "sub_cat_name": "Sub_Cat_1"
+        },
+        {
+          "sub_cat_id": 2,
+          "sub_cat_name": "Sub_Cat_2"
+        }
+      ]
+    },
+    {
+      "category_id": 2,
+      "category_name": "CATEGORY_2",
+      "sub_category": [
+        {
+          "sub_cat_id": 1,
+          "sub_cat_name": "Sub_Cat_21"
+        },
+        {
+          "sub_cat_id": 2,
+          "sub_cat_name": "Sub_Cat_22"
+        }
+      ]
+    }
+  ]
+}
