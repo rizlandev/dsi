2aed55d839a9b62234fc22dee88c26443bae4191
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-20 10:36:40 +0530

    Create credit_limit_period.php

---
+++ credit_limit_period.php
@@ -0,0 +1,13 @@
+{
+  "result": true,
+  "credit_limit_period": [
+    {
+      "cr_peri_id": 1,
+      "cr_peri_lable": "one month"
+    },
+    {
+      "cr_peri_id": 2,
+      "cr_peri_lable": "two months"
+    }
+  ]
+}
