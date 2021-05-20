e7a0758e132399251d243c6a1990a5f75d9975d1
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-20 10:34:56 +0530

    Create credit_limit_values.php

---
+++ credit_limit_values.php
@@ -0,0 +1,13 @@
+{
+  "result": true,
+  "credit_limit_data": [
+    {
+      "cr_lim_id": 1,
+      "cr_lim_lable": "50,000.00"
+    },
+    {
+      "cr_lim_id": 2,
+      "cr_lim_lable": "100,000.00"
+    }
+  ]
+}
