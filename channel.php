1728eecddaf123710b06d7fabbf8cfce763a5ced
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-20 08:33:53 +0530

    Create channel.php

---
+++ channel.php
@@ -0,0 +1,28 @@
+{
+  "result": true,
+  "channel_data": [
+    {
+      "channel_id": 1,
+      "channel_name": "CHANNEL_1",
+      "sub_channel": [
+        {
+          "sub_chan_id": 1,
+          "sub_chan_name": "Sub_CHA_1"
+        },{
+          "sub_chan_id": 2,
+          "sub_chan_name": "Sub_CHA_2"
+        }
+      ]
+    },
+        {
+      "channel_id": 2,
+      "channel_name": "CHANNEL_2",
+      "sub_channel": [
+        {
+          "sub_chan_id": 10,
+          "sub_chan_name": "Sub_CHA_21"
+        }
+      ]
+    }
+  ]
+}
