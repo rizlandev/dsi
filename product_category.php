2d6d068bac6556c3f553a5ae56105a8b56637054
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-24 17:07:59 +0530

    Create product_category.php

---
+++ product_category.php
@@ -0,0 +1,26 @@
+{
+    "result": true,
+    "product_cat_data": [
+        {
+            "cat_id": 5100,
+            "cat_code": "x",
+            "cat_name": "FLIP FLOPS"
+        },
+        {
+            "cat_id": 5101,
+            "cat_code": "xx",
+            "cat_name": "GENTS SLIPPERS AND SANDLES"
+        },
+        {
+            "cat_id": 5102,
+            "cat_code": "xxx",
+            "cat_name": "LADIES FOOTWARE"
+        },
+        {
+            "cat_id": 5103,
+            "cat_code": "xxxx",
+            "cat_name": "INFANT FOOTWARE"
+        }
+    ],
+    "count": 4
+}
