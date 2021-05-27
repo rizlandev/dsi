0684ee480ae9f7fd0b9d858c3c1ad2677211d527
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-05-27 13:30:13 +0530

    Update product_category.php
    updated with category

--- product_category.php
+++ product_category.php
@@ -1,26 +1,48 @@
 {
-    "result": true,
-    "product_cat_data": [
+  "result": true,
+  "product_cat_data": [
+    {
+      "cat_id": 5100,
+      "cat_code": "x",
+      "cat_name": "FLIP FLOPS",
+      "sub_category": [
         {
-            "cat_id": 5100,
-            "cat_code": "x",
-            "cat_name": "FLIP FLOPS"
+          "sub_cat_id": 51007,
+          "sub_cat_name": "MEN"
         },
         {
-            "cat_id": 5101,
-            "cat_code": "xx",
-            "cat_name": "GENTS SLIPPERS AND SANDLES"
-        },
-        {
-            "cat_id": 5102,
-            "cat_code": "xxx",
-            "cat_name": "LADIES FOOTWARE"
-        },
-        {
-            "cat_id": 5103,
-            "cat_code": "xxxx",
-            "cat_name": "INFANT FOOTWARE"
+          "sub_cat_id": 51007,
+          "sub_cat_name": "WOMEN"
         }
-    ],
-    "count": 4
+      ]
+    },
+    {
+      "cat_id": 5101,
+      "cat_code": "xx",
+      "cat_name": "GENTS SLIPPERS AND SANDLES",
+      "sub_category": [
+        {
+          "sub_cat_id": 510107,
+          "sub_cat_name": "A"
+        },
+        {
+          "sub_cat_id": 510108,
+          "sub_cat_name": "B"
+        }
+      ]
+    },
+    {
+      "cat_id": 5102,
+      "cat_code": "xxx",
+      "cat_name": "LADIES FOOTWARE",
+      "sub_category": []
+    },
+    {
+      "cat_id": 5103,
+      "cat_code": "xxxx",
+      "cat_name": "INFANT FOOTWARE",
+      "sub_category": []
+    }
+  ],
+  "count": 4
 }
