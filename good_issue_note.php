4c3d3863bf63148d7c656c2c30faa4dc6836ed24
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-06-19 13:00:25 +0530

    Create good_issue_note.php

---
+++ good_issue_note.php
@@ -0,0 +1,36 @@
+{
+    "result": true,
+    "load_issue_date": "2021-06-18",
+    "current_timestamp": 1624086128,
+    "Lorry_id": 0,
+    "Lorry_code": "62",
+    "pro_stock_data": [
+        {
+            "issue_note_id": 29977,
+            "issue_note_code": "GI2106180002",
+            "issue_note_date": "2021-06-18 08:41:01",
+            "stock": [
+                {
+                    "product_id": 3,
+                    "batch_id": 0,
+                    "batch_code": "",
+                    "batch_stock": "100",
+                    "batch_gross_stock": "100",
+                    "packets": 0,
+                    "boxes": 0
+                },
+                {
+                    "product_id": 10,
+                    "batch_id": 0,
+                    "batch_code": "",
+                    "batch_stock": "94",
+                    "batch_gross_stock": "100",
+                    "packets": 0,
+                    "boxes": 0
+                }
+            ]
+        }
+       
+    ],
+    "count": 1
+}
