2462ac1c38f6bc7b86b5ea2b71dabe74b351a884
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-06-19 12:25:49 +0530

    Update product_category.php
    update now

--- product_category.php
+++ product_category.php
@@ -1,23 +1,24 @@
+  
 {
   "result": true,
   "product_cat_data": [
     {
-      "cat_id": 3,
+      "cat_id": 5100,
       "cat_code": "x",
       "cat_name": "FLIP FLOPS",
       "sub_category": [
         {
-          "sub_cat_id": 1,
+          "sub_cat_id": 51007,
           "sub_cat_name": "MEN",
           "style": [
             {
-              "style_id": 1,
+              "style_id": 0401,
               "style_no": "PI0401",
               "style_desc": "FREEDOM PI0401-ASSTD",
               "style_img_url": "https://i.ibb.co/Tw5f3m4/PI0401.jpg"
             },
             {
-              "style_id": 2,
+              "style_id":0402,
               "style_no": "PI0402",
               "style_desc": "FREEDOM PI0402-ASSTD",
               "style_img_url": "https://i.ibb.co/44QGvMQ/PI0402.jpg"
@@ -25,17 +26,17 @@
           ]
         },
         {
-          "sub_cat_id": 2,
+          "sub_cat_id": 51008,
           "sub_cat_name": "WOMEN",
           "style": [
             {
-              "style_id": 3,
+              "style_id": 0130,
               "style_no": "GF0130",
               "style_desc": "BEAT GF0130-ASSTD",
               "style_img_url": "https://i.ibb.co/dtXXpkR/GF0130.jpg"
             },
             {
-              "style_id": 4,
+              "style_id": 0131,
               "style_no": "GF0131",
               "style_desc": "BEAT GF0131-ASSTD",
               "style_img_url": "https://i.ibb.co/xhTnMYG/GF0131.jpg"
@@ -45,22 +46,22 @@
       ]
     },
     {
-      "cat_id": 2,
+      "cat_id": 5200,
       "cat_code": "xx",
       "cat_name": "GENTS SLIPPERS AND SANDLES",
       "sub_category": [
         {
-          "sub_cat_id": 4,
+          "sub_cat_id": 52007,
           "sub_cat_name": "A",
           "style": [
             {
-              "style_id": 10,
+              "style_id": 0530,
               "style_no": "GS0530",
               "style_desc": "SLIP GS0530-ASSTD",
               "style_img_url": "https://homepages.cae.wisc.edu/~ece533/images/fruits.png"
             },
             {
-              "style_id": 11,
+              "style_id": 0531,
               "style_no": "GS0531",
               "style_desc": "SLIP GS0531-ASSTD",
               "style_img_url": "https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
@@ -68,20 +69,20 @@
           ]
         },
         {
-          "sub_cat_id": 4,
+          "sub_cat_id": 52008,
           "sub_cat_name": "B",
           "style": []
         }
       ]
     },
     {
-      "cat_id": 5102,
+      "cat_id": 5300,
       "cat_code": "xxx",
       "cat_name": "LADIES FOOTWARE",
       "sub_category": []
     },
     {
-      "cat_id": 5103,
+      "cat_id": 5400,
       "cat_code": "xxxx",
       "cat_name": "INFANT FOOTWARE",
       "sub_category": []
