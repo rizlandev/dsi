14cf04857409498e198d49e8b49f03c25f978c7d
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-06-21 00:11:50 +0530

    Create price_list.php
    added

---
+++ price_list.php
@@ -0,0 +1,19 @@
+{
+  "result": true,
+  "product_price": [
+    {
+      "item_id": 3,
+      "pri_unique_code": "XB00190",
+      "price_without_tax": "246.30",
+      "vat_amt": "19.70",
+      "price_include_tax": "266.00"
+    },
+    {
+      "item_id": 4,
+      "pri_unique_code": "XB00191",
+      "price_without_tax": "250.50",
+      "vat_amt": "19.50",
+      "price_include_tax": "270.00"
+    }
+  ]
+}
