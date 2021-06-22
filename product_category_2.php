8bc92f5d59a3070629050a03d2d750bbd20d6ef1
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-06-22 13:23:43 +0530

    Create product_category_2.php
    xcx

---
+++ product_category_2.php
@@ -0,0 +1,93 @@
+  
+{
+  "result": true,
+  "product_cat_data": [
+    {
+      "cat_id": 5100,
+      "cat_code": "x",
+      "cat_name": "FLIP FLOPS",
+      "sub_category": [
+        {
+          "sub_cat_id": 51007,
+          "sub_cat_name": "MEN",
+          "style": [
+            {
+              "style_id": 0401,
+              "style_no": "PI0401",
+              "style_desc": "FREEDOM PI0401-ASSTD",
+              "style_img_url": "https://i.ibb.co/Tw5f3m4/PI0401.jpg"
+            },
+            {
+              "style_id":0402,
+              "style_no": "PI0402",
+              "style_desc": "FREEDOM PI0402-ASSTD",
+              "style_img_url": "https://i.ibb.co/44QGvMQ/PI0402.jpg"
+            }
+          ]
+        },
+        {
+          "sub_cat_id": 51008,
+          "sub_cat_name": "WOMEN",
+          "style": [
+            {
+              "style_id": 0130,
+              "style_no": "GF0130",
+              "style_desc": "BEAT GF0130-ASSTD",
+              "style_img_url": "https://i.ibb.co/dtXXpkR/GF0130.jpg"
+            },
+            {
+              "style_id": 0131,
+              "style_no": "GF0131",
+              "style_desc": "BEAT GF0131-ASSTD",
+              "style_img_url": "https://i.ibb.co/xhTnMYG/GF0131.jpg"
+            },  
+{
+              "style_id": 0132,
+              "style_no": "GF0132",
+              "style_desc": "BEAT GF0132-ASSTD",
+              "style_img_url": ""
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "cat_id": 5200,
+      "cat_code": "xx",
+      "cat_name": "GENTS SOCKS",
+      "sub_category": [
+        {
+          "sub_cat_id": 51006,
+          "sub_cat_name": "ADULT UNISEX",
+          "style": [    
+            {
+              "style_id": 0019,
+              "style_no": "YV0019",
+              "style_desc": "3013A105.001-PER-BLACK",
+              "style_img_url": ""
+            },
+            {
+              "style_id":0020,
+              "style_no": "YV0020",
+              "style_desc": "3013A105.410-PEB-BLUE",
+              "style_img_url": ""
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "cat_id": 5102,
+      "cat_code": "xxx",
+      "cat_name": "LADIES FOOTWEAR",
+      "sub_category": []
+    },
+    {
+      "cat_id": 5302,
+      "cat_code": "xxxx",
+      "cat_name": "VESTS",
+      "sub_category": []
+    }
+  ],
+  "count": 4
+}
