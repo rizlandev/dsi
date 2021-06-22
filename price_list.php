091e3da4868adef286c68785f3f37542913fd2ef
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-06-22 09:36:41 +0530

    Update price_list.php
    updated

--- price_list.php
+++ price_list.php
@@ -14,6 +14,48 @@
       "price_without_tax": "250.50",
       "vat_amt": "19.50",
       "price_include_tax": "270.00"
+    },
+    {
+      "item_id": 5,
+      "pri_unique_code": "XB00192",
+      "price_without_tax": "250.30",
+      "vat_amt": "19.70",
+      "price_include_tax": "270.00"
+    },
+    {
+      "item_id": 6,
+      "pri_unique_code": "XB00193",
+      "price_without_tax": "255.50",
+      "vat_amt": "19.50",
+      "price_include_tax": "275.00"
+    },
+    {
+      "item_id": 7,
+      "pri_unique_code": "XB00194",
+      "price_without_tax": "255.30",
+      "vat_amt": "19.70",
+      "price_include_tax": "275.00"
+    },
+    {
+      "item_id": 8,
+      "pri_unique_code": "XB00195",
+      "price_without_tax": "260.50",
+      "vat_amt": "19.50",
+      "price_include_tax": "280.00"
+    },
+    {
+      "item_id": 9,
+      "pri_unique_code": "XB00196",
+      "price_without_tax": "275.30",
+      "vat_amt": "19.70",
+      "price_include_tax": "295.00"
+    },
+    {
+      "item_id": 10,
+      "pri_unique_code": "XB00197",
+      "price_without_tax": "300.50",
+      "vat_amt": "19.50",
+      "price_include_tax": "320.00"
     }
   ]
 }
