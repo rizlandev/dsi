2965ed0b287eb2cb789194558ad8994ad8d3649d
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-01 13:50:07 +0530

    Update price_list.php
    updated

--- price_list.php
+++ price_list.php
@@ -2,60 +2,28 @@
   "result": true,
   "product_price": [
     {
-      "item_id": 3,
-      "pri_unique_code": "XB00190",
-      "price_without_tax": "246.30",
-      "vat_amt": "19.70",
-      "price_include_tax": "266.00"
+      "item_id": 1,
+      "catalog_no": "12311708",
+      "pri_code_dss": "AAR-DEL001",
+      "dss_p_without_tax": "760.00",
+      "dss_vat_amt": "20.00",
+      "dss_p_include_tax": "780.00",
+      "pri_code_rto": "DSS-RTO009",
+      "rto_p_without_tax": "450.00",
+      "rto_vat_amt": "20.00",
+      "rto_p_include_tax": "470.00"
     },
     {
-      "item_id": 4,
-      "pri_unique_code": "XB00191",
-      "price_without_tax": "250.50",
-      "vat_amt": "19.50",
-      "price_include_tax": "270.00"
-    },
-    {
-      "item_id": 5,
-      "pri_unique_code": "XB00192",
-      "price_without_tax": "250.30",
-      "vat_amt": "19.70",
-      "price_include_tax": "270.00"
-    },
-    {
-      "item_id": 6,
-      "pri_unique_code": "XB00193",
-      "price_without_tax": "255.50",
-      "vat_amt": "19.50",
-      "price_include_tax": "275.00"
-    },
-    {
-      "item_id": 7,
-      "pri_unique_code": "XB00194",
-      "price_without_tax": "255.30",
-      "vat_amt": "19.70",
-      "price_include_tax": "275.00"
-    },
-    {
-      "item_id": 8,
-      "pri_unique_code": "XB00195",
-      "price_without_tax": "260.50",
-      "vat_amt": "19.50",
-      "price_include_tax": "280.00"
-    },
-    {
-      "item_id": 9,
-      "pri_unique_code": "XB00196",
-      "price_without_tax": "275.30",
-      "vat_amt": "19.70",
-      "price_include_tax": "295.00"
-    },
-    {
-      "item_id": 10,
-      "pri_unique_code": "XB00197",
-      "price_without_tax": "300.50",
-      "vat_amt": "19.50",
-      "price_include_tax": "320.00"
+      "item_id": 2,
+      "catalog_no": "12311709",
+      "pri_code_dss": "AAR-DEL002",
+      "dss_p_without_tax": "780.00",
+      "dss_vat_amt": "20.00",
+      "dss_p_include_tax": "800.00",
+      "pri_code_rto": "DSS-RTO010",
+      "rto_p_without_tax": "470.00",
+      "rto_vat_amt": "20.00",
+      "rto_p_include_tax": "490.00"
     }
   ]
 }
