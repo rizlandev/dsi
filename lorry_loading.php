7404b539ae6aeebd5e8f8d6fa6913d0cf70af725
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-02 13:20:51 +0530

    Create lorry_loading.php
    edit

---
+++ lorry_loading.php
@@ -0,0 +1,138 @@
+{
+    "result": true,
+    "code": 0,
+    "data": [
+        {
+            "loading_id": 1,
+            "loading_no": "LOD2021530936",
+            "vehicle_id": 1,
+            "distributor_id": 1,
+            "distributor_name": "Joe Dis Dis 01",
+            "status": "Approved/Accept",
+            "type": "Loading",
+            "created_at": "2021-06-30T06:22:34.000000Z",
+            "loding_products": [
+                {
+                    "batch_code": "20210630-1-8448",
+                    "product_id": 1,
+                    "product_catalog_no": "12311708",
+                    "product_catalog_desc": "TY 100*80-12 MC69 GENX DS G-A 08",
+                    "qty": 180
+                },
+                {
+                    "batch_code": "20210630-5-847A",
+                    "product_id": 5,
+                    "product_catalog_no": "GF013005",
+                    "product_catalog_desc": "BEAT GF0130-ASSTD 05",
+                    "qty": 500
+                },
+                {
+                    "batch_code": "20210630-6-848E",
+                    "product_id": 6,
+                    "product_catalog_no": "GF013006",
+                    "product_catalog_desc": "BEAT GF0130-ASSTD 06",
+                    "qty": 600
+                }
+            ]
+        },
+        {
+            "loading_id": 2,
+            "loading_no": "LOD2021530869",
+            "vehicle_id": 1,
+            "distributor_id": 1,
+            "distributor_name": "Joe Dis Dis 01",
+            "status": "Pending",
+            "type": "Loading",
+            "created_at": "2021-06-30T06:23:09.000000Z",
+            "loding_products": [
+                {
+                    "batch_code": "20210630-2-6772",
+                    "product_id": 2,
+                    "product_catalog_no": "12390121",
+                    "product_catalog_desc": "TY 90*100-10 MC69 GENX DS G- A 21",
+                    "qty": 290
+                }
+            ]
+        },
+        {
+            "loading_id": 3,
+            "loading_no": "LOD202161246",
+            "vehicle_id": 1,
+            "distributor_id": 1,
+            "distributor_name": "Joe Dis Dis 01",
+            "status": "Pending",
+            "type": "Loading",
+            "created_at": "2021-07-01T03:25:37.000000Z",
+            "loding_products": [
+                {
+                    "batch_code": "20210701-1-80B5",
+                    "product_id": 1,
+                    "product_catalog_no": "12311708",
+                    "product_catalog_desc": "TY 100*80-12 MC69 GENX DS G-A 08",
+                    "qty": 1800
+                },
+                {
+                    "batch_code": "20210701-2-80F6",
+                    "product_id": 2,
+                    "product_catalog_no": "12390121",
+                    "product_catalog_desc": "TY 90*100-10 MC69 GENX DS G- A 21",
+                    "qty": 2900
+                },
+                {
+                    "batch_code": "20210701-5-8115",
+                    "product_id": 5,
+                    "product_catalog_no": "GF013005",
+                    "product_catalog_desc": "BEAT GF0130-ASSTD 05",
+                    "qty": 5000
+                },
+                {
+                    "batch_code": "20210701-6-8130",
+                    "product_id": 6,
+                    "product_catalog_no": "GF013006",
+                    "product_catalog_desc": "BEAT GF0130-ASSTD 06",
+                    "qty": 6000
+                }
+            ]
+        },
+        {
+            "loading_id": 4,
+            "loading_no": "LOD202171879",
+            "vehicle_id": 1,
+            "distributor_id": 1,
+            "distributor_name": "Joe Dis Dis 01",
+            "status": "Pending",
+            "type": "Loading",
+            "created_at": "2021-07-01T11:08:55.000000Z",
+            "loding_products": [
+                {
+                    "batch_code": "20210701-1-8CC2",
+                    "product_id": 1,
+                    "product_catalog_no": "12311708",
+                    "product_catalog_desc": "TY 100*80-12 MC69 GENX DS G-A 08",
+                    "qty": 0
+                },
+                {
+                    "batch_code": "20210701-2-8D09",
+                    "product_id": 2,
+                    "product_catalog_no": "12390121",
+                    "product_catalog_desc": "TY 90*100-10 MC69 GENX DS G- A 21",
+                    "qty": 0
+                },
+                {
+                    "batch_code": "20210701-5-8D1F",
+                    "product_id": 5,
+                    "product_catalog_no": "GF013005",
+                    "product_catalog_desc": "BEAT GF0130-ASSTD 05",
+                    "qty": 0
+                },
+                {
+                    "batch_code": "20210701-6-8D31",
+                    "product_id": 6,
+                    "product_catalog_no": "GF013006",
+                    "product_catalog_desc": "BEAT GF0130-ASSTD 06",
+                    "qty": 0
+                }
+            ]
+        }
+    ]
+}
