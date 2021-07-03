29aee2e21e9e08c9ddf4d1c64fb0be6e4c6ab03f
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-03 16:57:02 +0530

    Update volume-discounts.php
    updated

--- volume-discounts.php
+++ volume-discounts.php
@@ -5,18 +5,13 @@
       "discount_schema_id": "21",
       "discount_lable": "DSS-SAMSON BRAND VOLUME DISCOUNT",
       "range": [
-        {
-          "lower_level": 0,
-          "upper_level": 0,
-          "discount_percent": 0
-        },
-        {
-          "lower_level": 100,
+         {
+          "lower_level": 1,
           "upper_level": 100,
           "discount_percent": 2
         },
         {
-          "lower_level": 300,
+          "lower_level": 101,
           "upper_level": 300,
           "discount_percent": 3
         }
