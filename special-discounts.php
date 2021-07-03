8548208a8cb77adba586a56eaed3b0866de0c5c7
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-03 17:11:27 +0530

    Create special-discounts.php
    added

---
+++ special-discounts.php
@@ -0,0 +1,85 @@
+{
+    "result": true,
+    "special_dis_schema": [
+        {
+            "discount_schema_id": 1,
+            "discount_lable": "spdis01",
+            "discount_type_id": 1,
+            "discount_type": "Brand Wise Discount",
+            "brand": [
+                {
+                    "brand_name": "5SRI",
+                    "discount_percent": 8,
+                    "paralle_items": []
+                },
+                {
+                    "brand_name": "5DSI",
+                    "discount_percent": 12,
+                    "paralle_items": []
+                }
+            ],
+            "category": [],
+            "style": []
+        },
+        {
+            "discount_schema_id": 2,
+            "discount_lable": "spdis2",
+            "discount_type_id": 2,
+            "discount_type": "Category Wise Discount",
+            "brand": [],
+            "category": [
+                {
+                    "cat_id": "5400",
+                    "discount_percent": 15,
+                    "paralle_items": []
+                },
+                {
+                    "cat_id": "5100",
+                    "discount_percent": 30,
+                    "paralle_items": []
+                }
+            ],
+            "style": []
+        },
+        {
+            "discount_schema_id": 3,
+            "discount_lable": "spdis3",
+            "discount_type_id": 3,
+            "discount_type": "Style Wise Discount",
+            "brand": [],
+            "category": [],
+            "style": [
+                {
+                    "style_no": "123117",
+                    "discount_percent": 8,
+                    "paralle_items": []
+                },
+                {
+                    "style_no": "GF0130",
+                    "discount_percent": 50,
+                    "paralle_items": []
+                }
+            ]
+        },
+        {
+            "discount_schema_id": 4,
+            "discount_lable": "zdsds",
+            "discount_type_id": 1,
+            "discount_type": "Brand Wise Discount",
+            "brand": [
+                {
+                    "brand_name": "5USFT",
+                    "discount_percent": 23,
+                    "paralle_items": []
+                },
+                {
+                    "brand_name": "5SAM",
+                    "discount_percent": 12,
+                    "paralle_items": []
+                }
+            ],
+            "category": [],
+            "style": []
+        }
+    ]
+}
