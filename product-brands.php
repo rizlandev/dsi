74c60a5e3b50b6badcc4a276db7af725d7a9b15f
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-03 17:15:05 +0530

    Create product-brands.php
    addes

---
+++ product-brands.php
@@ -0,0 +1,128 @@
+{
+    "result": true,
+    "product_brand": [
+        {
+            "brand": "5SRI",
+            "brand_desc": "SRI - BICYCLE TYRES & TUBES",
+            "category": [
+                {
+                    "sub_cat_id": "51805"
+                },
+                {
+                    "sub_cat_id": "51802"
+                },
+                {
+                    "sub_cat_id": "51801"
+                }
+            ]
+        },
+        {
+            "brand": "5SML",
+            "brand_desc": "SML - FOOTWEAR",
+            "category": [
+                {
+                    "sub_cat_id": "51008"
+                },
+                {
+                    "sub_cat_id": "51007"
+                }
+            ]
+        },
+        {
+            "brand": "5DSI",
+            "brand_desc": "DSI - FOOTWEAR",
+            "category": [
+                {
+                    "sub_cat_id": "51007"
+                },
+                {
+                    "sub_cat_id": "51009"
+                }
+            ]
+        },
+        {
+            "brand": "5SAM",
+            "brand_desc": "SAMSONS - FOOTWEAR",
+            "category": [
+                {
+                    "sub_cat_id": "51008"
+                },
+                {
+                    "sub_cat_id": "51007"
+                }
+            ]
+        },
+        {
+            "brand": "5TEND",
+            "brand_desc": "TENDER - FOOTWEAR, APPAREL & ACCESS",
+            "category": [
+                {
+                    "sub_cat_id": "51007"
+                }
+            ]
+        },
+        {
+            "brand": "5SOC",
+            "brand_desc": "SOCKS",
+            "category": [
+                {
+                    "sub_cat_id": "51009"
+                }
+            ]
+        },
+        {
+            "brand": "5MISC",
+            "brand_desc": "MISCELLANEOUS",
+            "category": [
+                {
+                    "sub_cat_id": "51999"
+                }
+            ]
+        },
+        {
+            "brand": "5USFT",
+            "brand_desc": "USOFTO - FOOTWEAR",
+            "category": [
+                {
+                    "sub_cat_id": "51007"
+                }
+            ]
+        },
+        {
+            "brand": "5AVI",
+            "brand_desc": "AVI - FOOTWEAR",
+            "category": [
+                {
+                    "sub_cat_id": "51002"
+                },
+                {
+                    "sub_cat_id": "51007"
+                }
+            ]
+        },
+        {
+            "brand": "5ASIC",
+            "brand_desc": "ASICS  FOOTWEAR & APPAREL & ACCES",
+            "category": [
+                {
+                    "sub_cat_id": "51009"
+                },
+                {
+                    "sub_cat_id": "51007"
+                },
+                {
+                    "sub_cat_id": null
+                },
+                {
+                    "sub_cat_id": "51002"
+                },
+                {
+                    "sub_cat_id": "51008"
+                },
+                {
+                    "sub_cat_id": "51006"
+                }
+            ]
+        }
+    ]
+}
