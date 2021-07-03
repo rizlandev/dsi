2f1342071c424f4629718eaa02200abdb92c39e5
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-03 17:12:43 +0530

    Create payment-discounts.php
    added

---
+++ payment-discounts.php
@@ -0,0 +1,23 @@
+{
+    "result": true,
+    "payment_dis_schema": [
+        {
+            "discount_schema_id": 1,
+            "discount_lable": "pymdis01",
+            "discount_type_id": 1,
+            "discount_type": "cash",
+            "range": [
+                {
+                    "min": "0.00",
+                    "max": "100.00",
+                    "discount_percent": 5
+                },
+                {
+                    "min": "101.00",
+                    "max": "200.00",
+                    "discount_percent": 7
+                }
+            ]
+        }
+    ]
+}
