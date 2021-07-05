7011d9e429b47b1ad3d4aea6baaeea94db9a2365
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-05 18:00:37 +0530

    Update volume-discounts.php
    updated with catalog data

--- volume-discounts.php
+++ volume-discounts.php
@@ -4,15 +4,30 @@
     {
       "discount_schema_id": "21",
       "discount_lable": "DSS-SAMSON BRAND VOLUME DISCOUNT",
+      "catalogs": [
+        {
+          "product_id": 3,
+          "catalog_no": "GF013003"
+        },
+        {
+          "product_id": 4,
+          "catalog_no": "GF013004"
+        }
+      ],
       "range": [
-         {
-          "lower_level": 1,
-          "upper_level": 100,
+        {
+          "lower_level": 0,
+          "upper_level": 99,
+          "discount_percent": 0
+        },
+        {
+          "lower_level": 100,
+          "upper_level": 299,
           "discount_percent": 2
         },
         {
-          "lower_level": 101,
-          "upper_level": 300,
+          "lower_level": 300,
+          "upper_level": 9999999,
           "discount_percent": 3
         }
       ]
