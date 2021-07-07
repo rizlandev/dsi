a53ab43c02255ab15ffc40762734b8905da6362c
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-07 12:20:57 +0530

    Create special-discount-outlets.php
    added

---
+++ special-discount-outlets.php
@@ -0,0 +1,28 @@
+{
+  "result": true,
+  "special_dis_outlet": [
+    {
+      "discount_schema_id": 1,
+      "valid_outlets": [
+        {
+          "outlet_id": 1
+        },
+        {
+          "outlet_id": 11
+        }
+      ]
+    },
+    {
+      "discount_schema_id": 2,
+      "valid_outlets": [
+        {
+          "outlet_id": 1
+        }
+      ]
+    },
+    {
+      "discount_schema_id": 3,
+      "valid_outlets": []
+    }
+  ]
+}
