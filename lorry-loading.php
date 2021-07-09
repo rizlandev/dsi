1ad9464f59574102d51ecdab846fe91ccf19953f
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-09 14:07:10 +0530

    Create lorry-loading.php

---
+++ lorry-loading.php
@@ -0,0 +1,30 @@
+{
+  "result": true,
+  "code": 0,
+  "data": {
+    "loading_id": 1,
+    "loading_no": "LD0001",
+    "vehicle_id": 1,
+    "distributor_id": 1,
+    "distributor_name": "Joe Don",
+    "status": "Approved/Accept",
+    "type": "Loading",
+    "created_at": "2021-06-20T04:17:26.000000Z",
+    "loding_products": [
+      {
+        "batch_code": "20210620-1-9241",
+        "product_id": 1,
+        "product_catalog_no": "12311708",
+        "product_catalog_desc": "TY 100*80-12 MC69 GENX DS G-A 08",
+        "qty": 20
+      },
+      {
+        "batch_code": "20210620-2-928B",
+        "product_id": 2,
+        "product_catalog_no": "12390121",
+        "product_catalog_desc": "TY 90*100-10 MC69 GENX DS G- A 21",
+        "qty": 30
+      }
+    ]
+  }
+}
