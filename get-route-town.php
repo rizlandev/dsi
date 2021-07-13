9568013fc1763ce1d5e7fbdd9c64b254f7ce5c33
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-13 09:33:40 +0530

    Create get-route-town.php
    addes

---
+++ get-route-town.php
@@ -0,0 +1,19 @@
+{
+  "result": true,
+  "town_data": [
+    {
+      "route_id": 1,
+      "route_name": "test route - 01",
+      "towns": [
+        {
+          "town_id": 1,
+          "town_name": "town_1"
+        },
+         {
+          "town_id": 2,
+          "town_name": "town_2"
+        
+      ]
+    }
+  ]
+}
