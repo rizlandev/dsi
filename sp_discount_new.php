919d260aea04167619651527ac3e13b76ff19af6
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-07-30 17:34:40 +0530

    Create sp_discount_new.php

---
+++ sp_discount_new.php
@@ -0,0 +1,39 @@
+{
+  "result": true,
+  "special_dis_schema": [
+    {
+      "discount_schema_id": 100,
+      "discount_lable": "28-07 Special Discount 1",
+      "discount_type_id": 1,
+      "discount_type": "Style Wise Discount",
+      "brand-category": [],
+      "style": [
+        {
+          "style_no": "GF0130",
+          "discount_percent": 2,
+          "paralle_items": []
+        },
+        {
+          "style_no": "PI0401",
+          "discount_percent": 3,
+          "paralle_items": []
+        }
+      ]
+    },
+    {
+      "discount_schema_id": 102,
+      "discount_lable": "28-07 Special Discount 2",
+      "discount_type_id": 2,
+      "discount_type": "BRAND CATEGORY COMBINED Discount",
+      "brand-category": [
+        {
+          "cat_id": "5100",
+          "brand_name": "5SML",
+          "discount_percent": 3,
+          "paralle_items": []
+        }
+      ],
+      "style": []
+    }
+  ]
+}
