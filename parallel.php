09c2a4079f050bad82d67df894520fb7e83d111b
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-08-19 07:37:26 +0530

    Create parallel.php
    add

---
+++ parallel.php
@@ -0,0 +1,18 @@
+{
+  "result": true,
+  "parallel_items": [
+    {
+      "catalog_no": "GF013004",
+      "substitute_cat_no": "AB261904"
+    },{
+      "catalog_no": "GF013004",
+      "substitute_cat_no": "AB261804"
+    },{
+      "catalog_no": "GF013005",
+      "substitute_cat_no": "AB261904"
+    },{
+      "catalog_no": "PI040124",
+      "substitute_cat_no": "AB261904"
+    }
+  ]
+}
