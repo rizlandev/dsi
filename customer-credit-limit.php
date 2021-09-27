c1b60347fa8ac4298fa057e684bb8788e525bae7
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-09-27 17:48:31 +0530

    Create customer-credit-limit.php
    add

---
+++ customer-credit-limit.php
@@ -0,0 +1,26 @@
+{
+  "result": true,
+  "customer_credit": [
+    {
+      "outletId": 42,
+      "outletName": "ABC shoes",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 38,
+      "outletName": "NEW customer 111",
+      "cl_allowed": 3000,
+      "cl_balance": 0,
+      "outstanding": 3000
+    },
+    {
+      "outletId": 60,
+      "outletName": "Saman Shoes",
+      "cl_allowed": 2500,
+      "cl_balance": 2500,
+      "outstanding": 0
+    }
+  ]
+}
