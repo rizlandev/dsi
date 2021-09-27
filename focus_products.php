c32745219cc0aea0196160ad56a1caa33517f7b0
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-09-27 15:25:52 +0530

    Create focus_products.php
    added

---
+++ focus_products.php
@@ -0,0 +1,17 @@
+{
+  "result": true,
+  "focus_products": [
+    {
+      "product_id": 3,
+      "catalog_no": "GF013003"
+    },
+    {
+      "product_id": 4,
+      "catalog_no": "GF013004"
+    },
+    {
+      "product_id": 5,
+      "catalog_no": "GF013005"
+    }
+  ]
+}
