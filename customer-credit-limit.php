d9d5897b747cbb28151db18fb86d774695939d7b
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-09-30 10:49:46 +0530

    Update customer-credit-limit.php
    updated

--- customer-credit-limit.php
+++ customer-credit-limit.php
@@ -4,6 +4,13 @@
     {
       "outletId": 42,
       "outletName": "ABC shoes",
+      "cl_allowed": 20000,
+      "cl_balance": 11000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 25,
+      "outletName": "Test Outlet 20",
       "cl_allowed": 10000,
       "cl_balance": 1000,
       "outstanding": 9000
@@ -11,16 +18,72 @@
     {
       "outletId": 38,
       "outletName": "NEW customer 111",
-      "cl_allowed": 3000,
-      "cl_balance": 0,
-      "outstanding": 3000
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 42,
+      "outletName": "ABC shoes",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 46,
+      "outletName": "ShoeMart",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 51,
+      "outletName": "Test out - Kris 2",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 54,
+      "outletName": "test rio1",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 56,
+      "outletName": "Fashion Mart",
+      "cl_allowed": 20000,
+      "cl_balance": 15000,
+      "outstanding": 5000
+    },
+    {
+      "outletId": 59,
+      "outletName": "Trendy Foots",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
     },
     {
       "outletId": 60,
       "outletName": "Saman Shoes",
-      "cl_allowed": 2500,
-      "cl_balance": 2500,
-      "outstanding": 0
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 62,
+      "outletName": "Shoe Point",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
+    },
+    {
+      "outletId": 72,
+      "outletName": "Rich Shoes",
+      "cl_allowed": 10000,
+      "cl_balance": 1000,
+      "outstanding": 9000
     }
   ]
 }
