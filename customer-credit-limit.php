9d0b4718adef24dd835105aa7366d0726a5503ae
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-10-05 12:04:37 +0530

    Update customer-credit-limit.php
    updated

--- customer-credit-limit.php
+++ customer-credit-limit.php
@@ -2,88 +2,298 @@
   "result": true,
   "customer_credit": [
     {
-      "outletId": 42,
-      "outletName": "ABC shoes",
-      "cl_allowed": 20000,
-      "cl_balance": 11000,
-      "outstanding": 9000
+      "outletId": 1570,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 25,
-      "outletName": "Test Outlet 20",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1571,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 38,
-      "outletName": "NEW customer 111",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1572,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 42,
-      "outletName": "ABC shoes",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1573,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 46,
-      "outletName": "ShoeMart",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1574,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 51,
-      "outletName": "Test out - Kris 2",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1575,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 54,
-      "outletName": "test rio1",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1576,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 56,
-      "outletName": "Fashion Mart",
-      "cl_allowed": 20000,
-      "cl_balance": 15000,
-      "outstanding": 5000
+      "outletId": 1577,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 59,
-      "outletName": "Trendy Foots",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1578,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 60,
-      "outletName": "Saman Shoes",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1579,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 62,
-      "outletName": "Shoe Point",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1580,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     },
     {
-      "outletId": 72,
-      "outletName": "Rich Shoes",
-      "cl_allowed": 10000,
-      "cl_balance": 1000,
-      "outstanding": 9000
+      "outletId": 1581,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1582,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1583,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1584,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1585,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1586,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1587,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1588,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1589,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1590,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1591,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1592,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1593,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1594,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1595,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1596,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1597,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1598,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1599,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1600,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1601,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1602,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1603,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1604,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1605,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1606,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1607,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1608,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1609,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1610,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
+    },
+    {
+      "outletId": 1611,
+      "outletName": "s",
+      "cl_allowed": 3000000,
+      "cl_balance": 3000000,
+      "outstanding": 0
     }
   ]
 }
