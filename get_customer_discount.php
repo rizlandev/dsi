313dd1b67d424b1527ce772536bf4f3d0628fc28
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-11-24 11:41:46 +0530

    Create get_customer_discount.php
    added

---
+++ get_customer_discount.php
@@ -0,0 +1,19 @@
+{
+  "result": "true",
+  "customer_discounts": [
+    {
+      "dis_id": "1",
+      "dis_label": "AAAA",
+      "customers": [
+        {
+          "customer_id": 5053,
+          "percentage": 5
+        },
+        {
+          "customer_id": 5052,
+          "percentage": 7.5
+        }
+      ]
+    }
+  ]
+}
