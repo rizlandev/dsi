4d2c4d622910070f7868a549383a29b27a950f31
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2022-12-05 15:28:12 +0530

    asds

--- outstanding-invoices.php
+++ outstanding-invoices.php
@@ -6,14 +6,16 @@
       "out_name": "BANDULA TEXTILES",
       "out_address": "NO 43. D.S SEENANAYAKA MAWATA,HIGURAKGODA,HIGURAKGODA,",
       "invs": [
-   {"i_date": "2021-10-11",
+            { 
+                    "i_date": "2021-10-11",
                     "i_time": "13:10:43",
                     "i_code": "DSI/INV/57/000005",
                     "i_id": 353,
                     "inv_total": 8287.6,
                     "payment": 0,
                     "outstanding": 8287.6,
-                    "type": 1}
+                    "type": 1
+            }
        
       ]
     }
