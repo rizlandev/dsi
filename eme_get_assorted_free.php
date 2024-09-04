b127a0f94683840c54fb436c5a218b49210876ef
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2024-09-04 18:07:04 +0530

    Create eme_get_assorted_free.php

---
+++ eme_get_assorted_free.php
@@ -0,0 +1,36 @@
+{
+    "result": true,
+    "freeIssues": [
+        {
+            "freeIssueId": 86,
+            "freeLabel": "XXX tablets",
+            "freeIssueLevel": 2,      
+            "base": 1,                  
+            "freeCombination": 1,       
+            "freeControlledBy": 1,     
+            "purchaseItems": [
+                {
+                    "categoryId": 1,
+                    "productId": 0,
+                    "batchId": 0,
+                    "productQty": 0,
+                    "weight": 0
+                }
+            ],
+            "freeItems": [
+                {
+                    "categoryId": 0,
+                    "productId": 23,
+                    "batchId": 1923,
+                    "weight": 0
+                }
+            ],
+            "freeDetails": [
+                {
+                    "purchaseQty": 15,
+                    "freeQty": 1,
+                    "lowerLimit": 30,
+                    "upperLimit": 120
+                }
+            ]
+        }]}
